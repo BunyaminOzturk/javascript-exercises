c433fda4a902d2c4e8855ff9134b795a9c9be3c3
Bunyamin Ozturk <bunyamin.ozturk.tr@gmail.com>
2025-04-27 11:18:41 +0300

    06 my solution

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,21 @@
-const leapYears = function() {
-
-};
+const leapYears = function(year) {
+    let div100 = year%100 == 0;
+    console.log(div100);
+    
+    let div400 = year%400 == 0;
+    console.log(div400);
+    
+    let div4 = year%4 == 0;
+    console.log(div4);
+    
+    if(div100 === true && div400 === false) return false;
+    if(div400 === true) return true;
+    if(div4 === true && div100 === false) {
+      return true;
+    } else {
+      return false;
+    }
+  };
 
 // Do not edit below this line
 module.exports = leapYears;

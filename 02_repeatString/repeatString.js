8ba039a325c3ddce479cf136ef247e649bbd3e52
Bunyamin Ozturk <bunyamin.ozturk.tr@gmail.com>
2025-04-27 10:55:48 +0300

    my solutions

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    if(num < 0){
+        return Error;
+    }
+    let string = "";
+    
+    for(i=1; i<=num; i++){
+        string += str;
+    }
+    return string;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,7 @@
-const removeFromArray = function() {
+const removeFromArray = function(ary, num) {
+    indexNum = ary.indexOf(num);
+    ary.splice(indexNum, 1);
+    return ary;
 };
 
 // Do not edit below this line

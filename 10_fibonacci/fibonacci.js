d3a950820b3373f3bbd07f1459388ae953440bb9
Bunyamin Ozturk <bunyamin.ozturk.tr@gmail.com>
2025-05-14 19:45:06 +0300

    10_fibonacci/fibonacci.js

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,10 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    let arr= [1,1];
+    for(let i=1; i<=num+1; i++){
+        arr.push(arr[i-1]+arr[i]);
+    }
+    //console.log(`${num}. eleman: ${arr[num-1]}`);
+    return arr[num-1];
 };
 
 // Do not edit below this line

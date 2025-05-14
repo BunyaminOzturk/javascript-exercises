0147de32f895e5011981fa46365ea6bcbf61f1b7
Bunyamin Ozturk <bunyamin.ozturk.tr@gmail.com>
2025-05-14 11:15:36 +0300

    09_palindromes/palindromes.js

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    str = str.replace(/[^a-zA-Z]/g, '');
+    let reverseStr = str.split("").reverse().join("");
+    if(str === reverseStr){
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
